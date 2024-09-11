f42969c83f9f70c8b5d8868f1b14a688653ea41a
Your Name <dmmuchai@gmail.com>
2024-09-11 11:27:20 -0700

    Task 16. ByteCode -> Python #2

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
     if a < b:
-        return c
-    elif b > c:
-        return a + b
+        return (c)
     else:
-        return a * b - c
+        if c > b:
+            return (a + b)
+        return (a * b - c)
