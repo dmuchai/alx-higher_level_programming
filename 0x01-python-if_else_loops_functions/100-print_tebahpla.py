b61beb9fbf9d4427d9b1e2c1f958a3f3a389736d
Your Name <dmmuchai@gmail.com>
2024-09-11 10:39:40 -0700

    Task 100-print_tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(26):
-    print("{}".format(chr(122 - i) if i % 2 == 0 else chr(122 - i - 32)), end="")
+for a in range(26):
+    print("{}".format(chr(122 - a) if a % 2 == 0 else chr(122 - a - 32)), end="")
