8ad5f53b2340ff41879dc23f0f143007fe402550
Your Name <dmmuchai@gmail.com>
2024-09-12 13:15:51 -0700

    Task 2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    count_args = len(sys.argv) - 1
+
+    if count_args == 0:
+        print("0 argumemts.")
+    elif count_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count_args))
+
+    for c in range(1, count_args + 1):
+        print("{}: {}".format(c, sys.argv[c]))
+if __name__ == "__main__":
+    main()
