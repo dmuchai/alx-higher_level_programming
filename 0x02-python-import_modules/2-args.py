e5721d80588bc3d28274e2df538abbdbf583b2ca
Your Name <dmmuchai@gmail.com>
2024-09-12 13:47:14 -0700

    Task 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     """print the no. and value of CLD"""
     import sys
 
-    count_args = len(sys.argv) - 1
-
-    if count_args == 0:
+    argv = sys.argv
+    b = len(argv) - 1
+    if b == 0:
         print("0 arguments.")
-    elif count_args == 1:
-        print("{} argument:".format(count_args))
-        print("{}: {}".format(count_args, argv[count_args]))
-    else:
-        print("{} arguments:".format(count_args))
-        for c in range(1, count_args + 1):
-            print("{}: {}".format(c, sys.argv[c]))
+    elif b == 1:
+        print("{} argument:".format(b))
+        print("{}: {}".format(b, argv[b]))
+    elif b > 1:
+        print("{} arguments:".format(b))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
