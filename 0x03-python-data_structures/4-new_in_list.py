54a5af73e7434574f32c8ec1e779edba55faf6e8
Your Name <dmmuchai@gmail.com>
2024-09-16 13:13:14 -0700

    Task 4-new_in_list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    
-    if idx < 0 or idx >= len(my_list):
+    listlen = len(my_list) - 1
+    if (idx < 0 or idx > listlen):
         return (my_list)
     else:
         new_list = my_list[:]
