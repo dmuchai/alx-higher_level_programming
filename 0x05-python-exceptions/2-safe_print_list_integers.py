794b4d210c59056c2ade08a89466e82868636a61
Your Name <dmmuchai@gmail.com>
2024-10-01 05:12:17 -0700

    Task 2. Print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     int_count = 0
-    for index in range(x):
+    for index in range(0, x):
         try:
             print("{:d}".format(my_list[index]), end="")
             int_count += 1
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError):
             continue
     print()
     return int_count
-
