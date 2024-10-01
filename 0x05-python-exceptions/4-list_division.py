2dc021572c98c5c0bc661d0c1812551b0fd85b0b
Your Name <dmmuchai@gmail.com>
2024-10-01 06:10:56 -0700

    Task 4. Divide a list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    div_list = []
+    for index in range(list_length):
+        try:
+            div_result = my_list_1[index] / my_list_2[index]
+        except ZeroDivisionError:
+            print("division by 0")
+            div_result = 0
+        except TypeError:
+            print("wrong type")
+            div_result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            div_list.append(div_result)
+    return div_list
