fd1aa13df55caf7d73994384bb3d5654f9269744
Your Name <dmmuchai@gmail.com>
2024-10-01 06:14:16 -0700

    Task 4. Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,7 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
             div_result = 0
         except IndexError:
             print("out of range")
-            result = 0
+            div_result = 0
         finally:
             div_list.append(div_result)
     return div_list
