b384d3daa7e38ac66e88c298c89276f325c9484d
Your Name <dmmuchai@gmail.com>
2024-10-02 11:26:56 -0700

    Task 2. Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines an empty class Square"""
+
+
+class Square:
+    """a class Square that defines a square.
+    """
+    def __init__(self, size=0):
+        """Creates new instances of square.
+
+        Args:
+            size: size of the square (1 side).
+        """
+
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
