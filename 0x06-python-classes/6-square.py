077e90d24d6e407b65e6f74425daba9eb424cf87
Your Name <dmmuchai@gmail.com>
2024-10-02 16:18:11 -0700

    Task 6. Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -75,7 +75,7 @@ class Square:
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-    
+
     def my_print(self):
         """prints to stdout the square with the # character
         """
