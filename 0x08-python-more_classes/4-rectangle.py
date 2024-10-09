1ba8d4d427ebc3dc5677c287076aa0327b81e0ea
Your Name <dmmuchai@gmail.com>
2024-10-09 13:03:40 -0700

    Task 4. Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -53,3 +53,6 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
+    def __repr__(self):
+        """Return a string representation of the rectangle to recreate it using eval"""
+        return f"Rectangle({self.__width}, {self.__height})"
