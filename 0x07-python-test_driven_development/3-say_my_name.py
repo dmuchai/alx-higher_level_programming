2e2f33144a417b2d68956ecb9ec5c448b407ec46
Your Name <dmmuchai@gmail.com>
2024-10-09 00:43:19 -0700

    Task 2. Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Defines a function that prints a name."""
-
+"""
+Defines a function that prints a name.
+"""
 
 def say_my_name(first_name, last_name=""):
     """Prints My name is <first name> <last name>.
@@ -15,4 +16,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
