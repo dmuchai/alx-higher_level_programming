2deb8c6f70caceb07327bf3648033ddd423dbf10
Your Name <dmmuchai@gmail.com>
2024-10-15 11:29:56 -0700

    Task 0. Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+"""
+This module provides a function to return the list of available attributes
+and methods of an object. It can be used to inspect the structure of an object.
+"""
+
+
 def lookup(obj):
-    """Return the list of available attributes and methods of an object."""
+    """
+    Return a list of available attributes and methods of an object.
+
+    This function takes any object as an argument and uses the built-in
+    `dir()` function to return a list of all attributes and methods associated
+    with the given object.
+
+    Args:
+        obj (any): The object whose attributes and methods are to be returned.
+
+    Returns:
+        list: A list of strings reps the attributes and methods.
+    """
     return dir(obj)
