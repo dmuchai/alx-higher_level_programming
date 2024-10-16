d415b95b7ef1908d73b1978718f8de8e85e9bbf0
Your Name <dmmuchai@gmail.com>
2024-10-16 00:38:15 -0700

    Task 1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,16 +4,16 @@ This module defines a class MyList that inherits from the built-in list class.
 It includes a method to print the list in sorted order.
 """
 
+
 class MyList(list):
     """
-    A subclass of list with an additional method to print the list sorted in ascending order.
+    A subclass of list with an method to print the list in ascending order.
     """
 
     def print_sorted(self):
         """
         Prints the elements of the list in ascending sorted order.
 
-        This method does not modify the original list; it simply prints a sorted
-        version of the list.
+        This method simply prints a sorted version of the list.
         """
         print(sorted(self))
