5ca4fa879362f12b96da286421979aa5756cb089
Your Name <dmmuchai@gmail.com>
2024-10-16 01:23:19 -0700

    Task 3. Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,5 +4,7 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """Return True if obj is an instance or an instance of a subclass of a_class."""
+    """
+    Return True if obj is instance or instance of subclass of a_class.
+    """
     return isinstance(obj, a_class)
