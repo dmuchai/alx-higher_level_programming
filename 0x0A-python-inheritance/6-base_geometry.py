5b50fa4e735c7366acd043504a9c2f1f10b44b87
Your Name <dmmuchai@gmail.com>
2024-10-16 06:48:14 -0700

    Task 6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A class BaseGeometry with a method that raises an exception"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry with a method that raises an exception."""
+
+
+    def area(self):
+        """Raises Exception showing that area is not implemented."""
+        raise Exception("area() is not implemented")
