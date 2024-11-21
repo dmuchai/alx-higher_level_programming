2edfda73e5c1330c31560fb29aa34f5508568638
Your Name <dmmuchai@gmail.com>
2024-11-21 08:16:56 -0800

    Task 17. Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,12 @@
+-- Script converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+-- Change the default character set and collation of the database
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Change the character set and collation of the table
+ALTER TABLE `hbtn_0c_0`.`first_table`
+    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Change the character set and collation of the 'name' field
+ALTER TABLE `hbtn_0c_0`.`first_table`
+    MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
