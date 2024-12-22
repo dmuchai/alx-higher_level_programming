2d127d06f78ce22fd773d493d581b4ff5d34b8a7
Your Name <dmmuchai@gmail.com>
2024-12-22 07:46:13 -0800

    Task 0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,21 +3,16 @@
 import MySQLdb
 from sys import argv
 
-# The code should not be executed when imported
 if __name__ == '__main__':
 
-    # make a connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
-    # It gives us the ability to have multiple seperate working environments
-    # through the same connection to the database.
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
 
-    rows = cur.fetchall()
+    rows = cursor.fetchall()
     for i in rows:
         print(i)
-    # Clean up process
-    cur.close()
+    cursor.close()
     db.close()
