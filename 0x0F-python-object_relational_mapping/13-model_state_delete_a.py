953812b37bb28c787ace791fbe1deb6c99af2140
Your Name <dmmuchai@gmail.com>
2024-12-23 13:23:00 -0800

    Task 13. Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""Deletes all State objects with a name containing the letter a."""
-from sys import argv
+"""
+Deletes all State objects with a name containing the letter a.
+"""
+
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            argv[1], argv[2], argv[3]
-        ),
-        pool_pre_ping=True
-    )
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states_to_delete = session.query(State).filter(
-            State.name.like('%a%')
-            ).all()
-    for state in states_to_delete:
-        session.delete(state)
+    Base.metadata.create_all(engine)
+    state_del = session.query(State).filter(State.name.like('%a%')).all()
+    for delete in state_del:
+        session.delete(delete)
     session.commit()
     session.close()
