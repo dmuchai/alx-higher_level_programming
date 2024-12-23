1bd7337efa3a12191d29625f9e68868a6b0742da
Your Name <dmmuchai@gmail.com>
2024-12-23 09:15:30 -0800

    Task 9. Contains a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,7 +15,12 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states_with_a = (
+            session.query(State)
+            .filter(State.name.like('%a%'))
+            .order_by(State.id)
+            .all()
+    )
 
     for state in states_with_a:
         print(f"{state.id}: {state.name}")
