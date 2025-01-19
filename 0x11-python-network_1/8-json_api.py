ef6ce1174a98204d7d4ca3cd9a2576be3eee7df3
Your Name <dmmuchai@gmail.com>
2025-01-19 09:32:17 -0800

    Task 8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -21,9 +21,11 @@ if __name__ == "__main__":
     try:
         json_response = response.json()
         if json_response:
-            print("[{}] {}".format(json_response.get("id"), json_response.get("name")))
+            print("[{}] {}".format(
+                json_response.get("id"),
+                json_response.get("name")
+            ))
         else:
             print("No result")
     except ValueError:
         print("Not a valid JSON")
-
