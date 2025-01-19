cbc60a8d3efa4508e1b4d999c9b79025fb179a38
Your Name <dmmuchai@gmail.com>
2025-01-19 09:42:04 -0800

    Task 9. My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script takes GitHub credentials (username and personal access token)
+and uses the GitHub API to display the user's id.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    response = requests.get(url, auth=(username, password))
+    if response.status_code == 200:
+        print(response.json().get("id"))
+    else:
+        print("None")
