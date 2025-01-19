b216de5b33875063eb4181ebe307bb3a60dda4af
Your Name <dmmuchai@gmail.com>
2025-01-19 06:49:54 -0800

    Task 3. Let me in!

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """
-script that takes in a URL, sends a request to the URL and displays the
-body of the response (decoded in utf-8).
-You have to manage urllib.error.HTTPError exceptions and
+This script takes in a URL, sends a request to the URL, and displays the body
+of the response (decoded in utf-8). It also handles HTTPError exceptions and
 prints the error code when such an exception occurs.
 """
+
 import urllib.request
 import urllib.error
 import sys
 
-
 if __name__ == "__main__":
     url = sys.argv[1]
+
     try:
         with urllib.request.urlopen(url) as response:
-            response_text = response.read().decode("utf-8")
-            print(response_text)
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        print(f"Error code: {e.code}")
