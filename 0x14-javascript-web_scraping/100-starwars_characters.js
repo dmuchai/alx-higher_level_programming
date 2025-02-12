f0880729389015f1c57ae2547a6d1085360c0641
Dennis Muniu Muchai <dmmuchai@gmail.com>
2025-02-12 11:08:51 -0800

    Task 7. Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// Script that prints all characters of a Star Wars movie.
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+
+    characters.forEach(characterUrl => {
+      request(characterUrl, (charErr, charResponse, charBody) => {
+        if (charErr) {
+          console.log(charErr);
+        } else {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        }
+      });
+    });
+  }
+});
