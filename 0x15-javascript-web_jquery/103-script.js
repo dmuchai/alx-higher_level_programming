5a4d8a40c9e00cba0de34a425db3895681e7bc69
dmuchai <dmmuchai@gmail.com>
2025-02-19 23:22:04 +0300

    updates to task 11and 12

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     function fetchTranslation() {
         const langCode = $("#language_code").val().trim();
         if (langCode !== "") {
-            const apiUrl = "https://hellosalut.stefanbohacek.dev/?lang=" + langCode;
+            const apiUrl = "https://www.fourtonfish.com/hellosalut/?lang=" + langCode;
             $.get(apiUrl, function (data) {
                 $("#hello").text(data.hello);
             }).fail(function () {
